45ba21b85b3175b92b747efa76571f21659a4832
Jai Madera <onesthathumbled@gmail.com>
2024-03-12 00:10:05 +0800

    subdomain

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:3000", "https://bonnie-client.netlify.app"
+    origins "http://localhost:3000", "https://bonnie-client.netlify.app", "https://bonnie.jairah.online"
 
     resource "*",
       headers: :any,
